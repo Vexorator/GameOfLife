49e5083b569f6fe1a75966e3f66e0212747a8551
Gabriel Kurowski <gabrys.kurowski@gmail.com>
2023-03-27 19:25:50 +0200

    new display func.

--- board.py
+++ board.py
@@ -1,13 +1,18 @@
-def init (width, height):
+def init (width: int, height: int):
     board = []
     for x in range(height):
         row = []
         for y in range(width):
-            row.append(" ")
+            row.append("0")
         board.append(row)
     return board
 
-def display(board):
+def display(board: list):
     print("\033[H\033[J", end="")
     for x in board:
-        print(x)
+        for y in x:
+            if y == "1":
+                print("█", end=" ")
+            else:
+                print(" ", end=" ")
+        print("", end="\n")
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from board import init, display
+import random
+import time
 
 WIDTH = 10
 HEIGHT = 10
 
-board = init(WIDTH, HEIGHT)
+while True:
+    board = init(WIDTH, HEIGHT)
 
-board[2][3] = "█"
-board[3][2] = "█"
-board[5][5] = "█"
 
-display(board)
+    numB = range(random.randint(2, 4))
+    for x in range(random.randint(2, 9)):
+        board[random.randrange(HEIGHT)][random.randrange(WIDTH)] = "1"
+
+
+    display(board)
+    time.sleep(1)
+    
